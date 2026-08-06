99e70615c17ddea31e80d0355b4d43f4510d7bb6
kartikdhyani817 <160416795+kartikdhyani817@users.noreply.github.com>
2026-08-06 16:45:17 +0530

    Add logger utility for logging pipeline events

---
+++ utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+from pathlib import Path
+
+
+LOG_FOLDER = Path("logs")
+LOG_FOLDER.mkdir(
+    exist_ok=True
+)
+
+LOG_FILE = LOG_FOLDER / "pipeline.log"
+
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s | %(levelname)s | %(message)s",
+)
+
+
+logger = logging.getLogger("RetailLakehouse")
